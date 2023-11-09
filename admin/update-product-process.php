714aaaf40f218513ba840e5007b9a3db15d76d4e
mahdi <rahnamamahdi78@gmail.com>
2023-11-09 11:46:32 +0330

    delete require from update-product-process & uncomment header in update-product-process

--- admin/update-product-process.php
+++ admin/update-product-process.php
@@ -1,10 +1,9 @@
 <?php
 require_once "Product.php";
-require_once "Category.php";
 $product=new product("products");
 $product->update(["title"=>$_POST["title"],
     "price"=>$_POST["price"],
     "inventory"=>$_POST["inventory"],
     "category"=>$_POST["category"],
     "id"=>$_POST["id"]]);
-//header("Location: products-panel");
+header("Location: products-panel");
